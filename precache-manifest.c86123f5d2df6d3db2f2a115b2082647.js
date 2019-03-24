c5d33df3c8ae9f10e951c0b21b3e7b7eea849792
Marcelo Finamor Vieira <marcelofinamorvieira@gmail.com>
2019-03-24 22:08:14 +0100

    Updates

---
+++ precache-manifest.c86123f5d2df6d3db2f2a115b2082647.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "15bb3101340a9b514097",
+    "url": "/to-do-list/static/js/runtime~main.9900d293.js"
+  },
+  {
+    "revision": "bbf8b1de7c19fecd4816",
+    "url": "/to-do-list/static/js/main.656b0d44.chunk.js"
+  },
+  {
+    "revision": "3ae3a01717630a9f9287",
+    "url": "/to-do-list/static/js/2.20e01d7c.chunk.js"
+  },
+  {
+    "revision": "bbf8b1de7c19fecd4816",
+    "url": "/to-do-list/static/css/main.c967a381.chunk.css"
+  },
+  {
+    "revision": "0e7e83bd102375679f1c05c32336bc4d",
+    "url": "/to-do-list/index.html"
+  }
+];
